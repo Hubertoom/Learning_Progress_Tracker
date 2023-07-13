7782f1f110abcc66d09a19fc4cb6e41bd1512711
Hubertoom <h.michalec@o2.pl>
2023-07-13 14:17:56 +0200

    stage_3 passed

--- Learning Progress Tracker (Java)/task/src/tracker/LearningTracker.java
+++ Learning Progress Tracker (Java)/task/src/tracker/LearningTracker.java
@@ -3,7 +3,7 @@ package tracker;
 import java.util.*;
 
 public class LearningTracker {
-    private final Map<Integer, Student> studentsList = new LinkedHashMap<>();
+    private final Map<String, Student> studentsList = new LinkedHashMap<>();
     private final Scanner scanner = new Scanner(System.in);
 
     public void run() {
@@ -19,6 +19,8 @@ public class LearningTracker {
             switch (input) {
                 case "add students" -> addStudents();
                 case "list" -> displayStudentsList();
+                case "add points" -> addPoints();
+                case "find" -> findStudent();
                 case "exit" -> {
                     System.out.println("Bye");
                     return;
@@ -31,7 +33,75 @@ public class LearningTracker {
 
     }
 
+    private void findStudent() {
+        System.out.println("Enter an id or 'back' to return");
+        while (true) {
+            String id = scanner.nextLine();
+            if (id.equals("back")) {
+                return;
+            }
+
+            if (!studentsList.containsKey(id)) {
+                System.out.printf("No student is found for id=%s\n", id);
+                return;
+            }
+
+            System.out.println(studentsList.get(id));
+        }
+    }
+
+    private void addPoints() {
+        System.out.println("Enter an id and points or 'back' to return:");
+        while (true) {
+            String input = scanner.nextLine();
+            String[] command = input.split("\\s+");
+
+            if (command[0].equals("back")) {
+                break;
+            }
+
+            if (command.length != 5 || input.contains("-")) {
+                System.out.println("Incorrect points format");
+                continue;
+            }
+
+            int[] points;
+            try {
+                points = Arrays.stream(command).skip(1)
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
+            } catch (NumberFormatException e) {
+                System.out.println("Incorrect points format");
+                continue;
+            }
+
+            Optional<Student> studentOptional = studentsList.values()
+                    .stream().filter(student -> Objects.equals(student.getStudentId(), command[0]))
+                    .findAny();
+
+            if (studentOptional.isEmpty()) {
+                System.out.printf("No student is found for id=%s\n.", command[0]);
+                continue;
+            }
+
+            updatePoints(studentOptional.get(), points[0], points[1], points[2], points[3]);
+        }
+
+    }
+
+    private void updatePoints(Student student, int java, int dsa, int database, int spring) {
+        student.addJava(java);
+        student.addDsa(dsa);
+        student.addDatabase(database);
+        student.addSpring(spring);
+        System.out.println("Points updated");
+    }
+
     private void displayStudentsList() {
+        if (studentsList.isEmpty()) {
+            System.out.println("No students found");
+            return;
+        }
         System.out.println("Students:");
         studentsList.values().stream()
                 .map(Student::getStudentId)
--- Learning Progress Tracker (Java)/task/src/tracker/Student.java
+++ Learning Progress Tracker (Java)/task/src/tracker/Student.java
@@ -2,19 +2,25 @@ package tracker;
 
 public class Student {
     private static int currentId = 10_000;
-    private final int studentId;
+    private final String studentId;
     private final String name;
     private final String surname;
     private final String email;
 
+    // courses:
+    private int java = 0;
+    private int dsa = 0;
+    private int database = 0;
+    private int spring = 0;
+
     public Student(String name, String surname, String email) {
-        this.studentId = currentId++;
+        this.studentId = String.valueOf(currentId++);
         this.name = name;
         this.surname = surname;
         this.email = email;
     }
 
-    public int getStudentId() {
+    public String getStudentId() {
         return studentId;
     }
 
@@ -28,5 +34,27 @@ public class Student {
 
     public String getEmail() {
         return email;
+    }
+
+    public void addJava(int java) {
+        this.java += java;
+    }
+
+    public void addDsa(int dsa) {
+        this.dsa += dsa;
+    }
+
+    public void addDatabase(int database) {
+        this.database += database;
+    }
+
+    public void addSpring(int spring) {
+        this.spring += spring;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s points: Java=%d; DSA=%d; Databases=%d; Spring=%d"
+        ,studentId, java, dsa, database, spring);
     }
 }
